a490d2393b2411b294cf1bdad2e17f9c275f7019
knight <ariefmaulana130598@gmail.com>
2020-01-27 02:22:41 +0700

    Init and Direction Marker

--- app/src/main/java/com/example/androidmap/MainActivity.java
+++ app/src/main/java/com/example/androidmap/MainActivity.java
@@ -2,70 +2,89 @@ package com.example.androidmap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.maps.UiSettings;
-import com.wanderingcan.persistentsearch.PersistentSearchView;
-
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
 
     private MapView mapView;
     private Button btnmotor, btnmobil;
-    private PersistentSearchView psinitplace, psdirectionplace;
-    public int kendaraan = 0;
+    private EditText etinitplace, etdirectionplace;
+    public  static int kendaraan = 0;
+    public static int kode_cari = 0;
 
     private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
     private MapboxMap mapboxMap;
-    private CarmenFeature home;
-    private CarmenFeature work;
+    private String MAPBOX_API = BuildConfig.MAPBOX_API_KEY;
+    public CarmenFeature init, direction;
     private String geojsonSourceLayerId = "geojsonSourceLayerId";
     private String symbolIconId = "symbolIconId";
+    Symbol motorsymbol, mobilsymbol, tujuansymbol;
+    SymbolManager symbolManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Mapbox.getInstance(this, "pk.eyJ1IjoiYXJpZWYxMzA1OTgiLCJhIjoiY2s1NWNrbHloMDkzbzNucWkxYTJkMmpyOSJ9.gr6DVa3CzDJXrr667mRfvg");
+        Mapbox.getInstance(this, MAPBOX_API);
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-                        UiSettings uiSettings = mapboxMap.getUiSettings();
-                        uiSettings.setCompassGravity(Gravity.BOTTOM | Gravity.END);
-                        uiSettings.setCompassMargins(0, 0, 155, 16);
-                        System.out.println(uiSettings.getAttributionGravity());
-                    }
-                });
-            }
-        });
 
         btnmotor = findViewById(R.id.motor);
         btnmotor.setOnClickListener(this);
         btnmobil = findViewById(R.id.mobil);
         btnmobil.setOnClickListener(this);
-        psinitplace = findViewById(R.id.initplace);
-        psdirectionplace = findViewById(R.id.directionplace);
+        etinitplace = findViewById(R.id.initplace);
+        etinitplace.setOnClickListener(this);
+        etdirectionplace = findViewById(R.id.directionplace);
+        etdirectionplace.setOnClickListener(this);
     }
 
     @Override
@@ -97,6 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
+
         mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
@@ -106,12 +126,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 uiSettings.setCompassMargins(0, 0, 155, 16);
                 System.out.println(uiSettings.getAttributionGravity());
 
-                initSearchFab();
-                addUserLocations();
+                // Annotation / Marker
+                symbolManager = new SymbolManager(mapView, mapboxMap, style);
 
-                // Add the symbol layer icon to map for future use
-                style.addImage(symbolIconId, BitmapFactory.decodeResource(
-                        MainActivity.this.getResources(), R.drawable.ic_place_green_24dp));
+                symbolManager.setIconAllowOverlap(true);
+                symbolManager.setIconIgnorePlacement(true);
+
+                // Add symbol at specified lat/lon
+
+                Bitmap motorblue = getBitmapFromVectorDrawable(MainActivity.this, R.drawable.ic_motorcycle_blue_24dp);
+                style.addImage("motor_blue", motorblue);
+                Bitmap markerblue = getBitmapFromVectorDrawable(MainActivity.this, R.drawable.ic_place_blue_24dp);
+                style.addImage("marker_blue", markerblue);
+                Bitmap carblue = getBitmapFromVectorDrawable(MainActivity.this, R.drawable.ic_directions_car_blue_24dp);
+                style.addImage("car_blue", carblue);
+
+                initSearchFab();
 
                 // Create an empty GeoJSON source using the empty feature collection
                 setUpSource(style);
@@ -123,55 +153,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initSearchFab() {
-        psinitplace.setOnSearchListener(new PersistentSearchView.OnSearchListener() {
+        etinitplace.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSearchOpened() {
-                psinitplace.openSearch();
-            }
-
-            @Override
-            public void onSearchClosed() {
-                //Called when the searchbar is closed by the user or by something calling
-                //persistentSearchView.closeSearch();
-            }
-
-            @Override
-            public void onSearchCleared() {
-                //Called when the searchbar has been cleared by the user by removing all
-                //the text or hitting the clear button. This also will be called if
-                //persistentSearchView.populateSearchText() is set with a null string or
-                //an empty string
-            }
-
-            @Override
-            public void onSearchTermChanged(CharSequence term) {
-                //Called when the text in the searchbar has been changed by the user or
-                //by persistentSearchView.populateSearchText() with text passed in.
-                //Best spot to handle giving suggestions to the user in the menu
-            }
-
-            @Override
-            public void onSearch(CharSequence text) {
-                //Called when the user hits the IME Action Search on the keyboard to search
-                //Here is the best spot to handle searches
+            public void onClick(View v) {
+                kode_cari = 0;
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : MAPBOX_API)
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(MainActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
             }
         });
-    }
 
-    private void addUserLocations() {
-        home = CarmenFeature.builder().text("Mapbox SF Office")
-                .geometry(Point.fromLngLat(-122.3964485, 37.7912561))
-                .placeName("50 Beale St, San Francisco, CA")
-                .id("mapbox-sf")
-                .properties(new JsonObject())
-                .build();
-
-        work = CarmenFeature.builder().text("Mapbox DC Office")
-                .placeName("740 15th Street NW, Washington DC")
-                .geometry(Point.fromLngLat(-77.0338348, 38.899750))
-                .id("mapbox-dc")
-                .properties(new JsonObject())
-                .build();
+        etdirectionplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode_cari = 1;
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : MAPBOX_API)
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(MainActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
     }
 
     private void setUpSource(@NonNull Style loadedMapStyle) {
@@ -189,32 +199,96 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            if(kode_cari == 0){
+                init = PlaceAutocomplete.getPlace(data);
+                etinitplace.setText(init.placeName());
 
-            // Retrieve selected location's CarmenFeature
-            CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
+                if (mapboxMap != null) {
+                    Style style = mapboxMap.getStyle();
+                    if (style != null) {
+                        // Annotation / Marker
+                        if(kendaraan == 0){
+                            if(motorsymbol != null){
+                                symbolManager.delete(motorsymbol);
+                            }
 
-            // Create a new FeatureCollection and add a new Feature to it using selectedCarmenFeature above.
-            // Then retrieve and update the source designated for showing a selected location's symbol layer icon
+                            if(mobilsymbol != null){
+                                symbolManager.delete(mobilsymbol);
+                            }
+                            // Add symbol at specified lat/lon
+                            motorsymbol = symbolManager.create(new SymbolOptions()
+                                    .withLatLng(new LatLng(((Point) init.geometry()).latitude(),
+                                            ((Point) init.geometry()).longitude()))
+                                    .withIconImage("motor_blue")
+                                    .withIconSize(1.0f));
+                        }else{
+                            if(motorsymbol != null){
+                                symbolManager.delete(motorsymbol);
+                            }
 
-            if (mapboxMap != null) {
-                Style style = mapboxMap.getStyle();
-                if (style != null) {
-                    GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
-                    if (source != null) {
-                        source.setGeoJson(FeatureCollection.fromFeatures(
-                                new Feature[] {Feature.fromJson(selectedCarmenFeature.toJson())}));
+                            if(mobilsymbol != null){
+                                symbolManager.delete(mobilsymbol);
+                            }
+                            // Add symbol at specified lat/lon
+                            Symbol mobil = symbolManager.create(new SymbolOptions()
+                                    .withLatLng(new LatLng(((Point) init.geometry()).latitude(),
+                                            ((Point) init.geometry()).longitude()))
+                                    .withIconImage("car_blue")
+                                    .withIconSize(1.0f));
+                        }
+
+                        // Move map camera to the selected location
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                                new CameraPosition.Builder()
+                                        .target(new LatLng(((Point) init.geometry()).latitude(),
+                                                ((Point) init.geometry()).longitude()))
+                                        .zoom(13)
+                                        .build()), 3000);
                     }
+                }
+            }else{
+                direction = PlaceAutocomplete.getPlace(data);
+                etdirectionplace.setText(direction.placeName());
 
-                    // Move map camera to the selected location
-                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
-                            new CameraPosition.Builder()
-                                    .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
-                                            ((Point) selectedCarmenFeature.geometry()).longitude()))
-                                    .zoom(14)
-                                    .build()), 4000);
+                if (mapboxMap != null) {
+                    Style style = mapboxMap.getStyle();
+                    if (style != null) {
+                        if(tujuansymbol != null){
+                            symbolManager.delete(tujuansymbol);
+                        }
+
+                        // Add symbol at specified lat/lon
+                        tujuansymbol = symbolManager.create(new SymbolOptions()
+                                .withLatLng(new LatLng(((Point) direction.geometry()).latitude(),
+                                        ((Point) direction.geometry()).longitude()))
+                                .withIconImage("marker_blue")
+                                .withIconSize(1.0f));
+
+                        // Move map camera to the selected location
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                                new CameraPosition.Builder()
+                                        .target(new LatLng(((Point) direction.geometry()).latitude(),
+                                                ((Point) direction.geometry()).longitude()))
+                                        .zoom(13)
+                                        .build()), 3000);
+                    }
                 }
             }
         }
+    }
+
+    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+
+        Bitmap bitmap = null;
+        if (drawable != null) {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+        }
+        return bitmap;
     }
 
     @Override
